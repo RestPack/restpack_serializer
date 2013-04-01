ee5099b42cf59e158b4cba7b54528b4bf1ba012e
Gavin Joyce <gavinjoyce@gmail.com>
2013-04-01 15:57:17 +0100

    removed unneeded code

--- lib/restpack-serializer/serializable.rb
+++ lib/restpack-serializer/serializable.rb
@@ -3,14 +3,12 @@ require_relative "serializable/attributes"
 module RestPack
   module Serializable    
     def self.included(base)
-      base.extend(ClassMethods)
       base.extend(RestPack::Serializable::Attributes)
       super
     end
 
     def as_json(model, options = {})
-      @model = model
-      @options = options
+      @model, @options = model, options
 
       data = {}
       self.class.serializable_attributes.each do |key, name|
@@ -21,10 +19,6 @@ module RestPack
 
     def include_attribute?(name)
       self.send("include_#{name}?".to_sym)
-    end
-
-    module ClassMethods
-
     end
   end
 end
