e13295bf144e4d6a722fedb77fa3f3ae1fac1fc0
Gavin Joyce <gavinjoyce@gmail.com>
2013-04-14 18:57:56 +0100

    removed unused require statements as we no longer depend on active model

--- lib/restpack_serializer.rb
+++ lib/restpack_serializer.rb
@@ -1,8 +1,3 @@
-require 'active_model'
-require 'active_model/array_serializer'
-require 'active_model/serializer'
-require 'active_model/serializer/associations'
-
 require 'will_paginate'
 require 'will_paginate/active_record'
 
