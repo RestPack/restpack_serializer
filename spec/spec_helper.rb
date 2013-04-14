a382697228015382cc09fb2f62d5f0915f3495c3
Gavin Joyce <gavinjoyce@gmail.com>
2013-04-14 18:54:24 +0100

    removed unused require statements as we no longer depend on active model

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,3 @@
-require 'active_model'
-require "active_model/array_serializer"
-require "active_model/serializer"
-require "active_model/serializer/associations"
-
 require './lib/restpack_serializer'
 require './spec/fixtures/db'
 require './spec/fixtures/serializers'
