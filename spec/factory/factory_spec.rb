a7707741e02fab3b4bc9eeaf2ceb2573f8adf6b1
Gavin Joyce <gavinjoyce@gmail.com>
2013-04-14 19:37:13 +0100

    shorter spec descriptions

--- spec/factory/factory_spec.rb
+++ spec/factory/factory_spec.rb
@@ -3,13 +3,13 @@ require './spec/spec_helper'
 describe RestPack::Serializer::Factory do
   let(:factory) { RestPack::Serializer::Factory }
 
-  it "creates a serializer by string" do
+  it "creates by string" do
     factory.create("Song").should be_an_instance_of(SongSerializer)
   end
-  it "creates a serializer by symbol" do
+  it "creates by symbol" do
     factory.create(:song).should be_an_instance_of(SongSerializer)
   end
-  it "creates a serializer by class" do
+  it "creates by class" do
     factory.create(Song).should be_an_instance_of(SongSerializer)
   end
 end
