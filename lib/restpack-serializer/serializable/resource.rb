f37c5ccf79a47349fd27f9f3dccfb82021bf904f
Gavin Joyce <gavinjoyce@gmail.com>
2013-05-18 12:43:44 +0100

    removed stray comment

--- lib/restpack-serializer/serializable/resource.rb
+++ lib/restpack-serializer/serializable/resource.rb
@@ -3,7 +3,6 @@ module RestPack::Serializer::Resource
 
   module ClassMethods
     def resource(params = {}, scope = nil)
-      #TODO: GJ: create ResourceOptions and PageOptions classes
       page_with_options RestPack::Serializer::Options.new(self.model_class, params, scope)
     end
   end
