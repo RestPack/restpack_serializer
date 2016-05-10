e3acf76fb60c89526febd2b98fd94a7d3e2d3ef6
Gavin Joyce <gavinjoyce@gmail.com>
2016-05-10 08:41:31 +0100

    added benchmark for more complex serializer

--- performance/perf.rb
+++ performance/perf.rb
@@ -6,7 +6,14 @@ class SimpleSerializer
   attributes :id, :title
 end
 
-iterations = 180_000 #180_000 ~> 1 second
+class ComplexSerializer
+  include RestPack::Serializer
+
+  attributes :a, :b, :c, :d, :e, :f, :g, :h, :i, :j, :k, :l, :m, :n, :o, :p, :q, :r, :s, :t
+end
+
+iterations = 180_000
+
 Benchmark.bm(22) do |bm|
   bm.report('simple serializer') do
 
@@ -17,6 +24,18 @@ Benchmark.bm(22) do |bm|
 
     iterations.times do
       SimpleSerializer.as_json(model)
+    end
+  end
+
+  bm.report('complex serializer') do
+
+    model = {
+      a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10,
+      k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18, s: 19, t: 20,
+    }
+
+    iterations.times do
+      ComplexSerializer.as_json(model)
     end
   end
 end
